804ff9472e5823042d889376751b755786b0d85e
rodrigo-tamagusku <rodrigo.tamagusku@gmail.com>
2025-08-13 15:54:47 -0300

    Leitura inicial da página

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ScraperBooks
+
+Aplicação em C# que faz scraping do site https://books.toscrape.com/,
+aplica filtros configuráveis e envia os dados filtrados para um endpoint simulado de API REST.
+
+Features:
+
+* Scraping
+* Filtros configuráveis
+* Normalização e exportação
+* Integração com API REST
+* Implementação em C#.
+* Tratamento de erros e logging mínimo.
+* Uso de biblioteca de parsing HTML
+* Saída em JSON e XML.
+* Documentação clara no README.md (passo a passo de configuração e execução)
--- ScraperBooks/ScraperBooks/Program.cs
+++ ScraperBooks/ScraperBooks/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string URL_BOOKS = "https://books.toscrape.com/";
+        HttpClient httpClient = new();
+        string html = httpClient.GetStringAsync(URL_BOOKS).GetAwaiter().GetResult();
+        Console.WriteLine(html);
+        HtmlDocument documento = new();
+        documento.LoadHtml(html);
+        Console.WriteLine(documento.ToString());
+    }
+}
